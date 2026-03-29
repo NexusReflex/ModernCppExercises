2b7b57f8ffe6a2897c8ae8a48bdcaeb4aa3fb1ff
NexusReflex <lbielenberg@anybotics.com>
2026-03-29 11:42:07 +0200

    added is_alive flag

--- Functional/src/main.cpp
+++ Functional/src/main.cpp
@@ -8,15 +8,19 @@
 int main(){
 
     int total_damage = 0;
+    bool is_alive = true;
+
     std::string panda_attack = "Panda";
     std::string grizzly_attack = "Grizzly";
-    
-    auto attack_callback = [&total_damage](const std::string& attacker, int damage_taken){
+
+    auto attack_callback = [&total_damage, &is_alive](const std::string& attacker, int damage_taken){
+        if (!is_alive) return; // Don't process if already dead
         total_damage += damage_taken;
         if (total_damage < 100){
             std::cout << "You got attacked by a " << attacker << ". Total damage taken: " << total_damage << std::endl;
         }
         else{
+            is_alive = false;
             std::cout << "Game-over! You got attacked by a " << attacker << " again. Total damage: " << total_damage << "\nCongrats, now you're dead\n";
         }
     };
@@ -26,12 +30,10 @@ int main(){
     manager.subscribe(panda_attack, attack_callback);
     manager.subscribe(grizzly_attack, attack_callback);
 
-    manager.emit(grizzly_attack, 25);
-    manager.emit(panda_attack, 1);
-    manager.emit(grizzly_attack, 25);
-    manager.emit(grizzly_attack, 25);
-    manager.emit(panda_attack, 1);
-    manager.emit(grizzly_attack, 25);
-
+    while(is_alive) {
+        manager.emit(grizzly_attack, 25);
+        manager.emit(panda_attack, 1);
+    }
+    
 
 }
